a0d2dfde0255eea18e442cdd6f498021d33b84e2
sadhvika <rsadhvika@gmail.com>
2021-06-09 21:24:25 +0530

    webhook fulfillment

--- server.js
+++ server.js
@@ -1,54 +1,78 @@
-const express = require('express');
-const app = express();
+//Use app in strict mode
+'use strict';
 
-app.use(express.static(__dirname));
+//Using actions on google library
+const {
+  dialogflow,
+} = require('actions-on-google');
 
-var PORT = process.env.PORT || 9000;
-var PAUSE = false;
-var last_data = [];
-var dummy = [{temp:0, time: get_time()},{temp:0, time: get_time()*1 + 10}];
+//Using expressjs
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require("request");
 
-app.post('/whhandler',function(req, res) {
-  console.log(req.res);
-  last_data.push(req.res)
-  res.send()
-})
-app.get('/fetch',function(req, res) {
-  if(last_data.length) {
-    res.send(last_data)
-  }else {
-    res.send(dummy)
-  }
-})
+const app = dialogflow({debug: true});
+var expressApp = express();
 
-app.get('/clear',function(req, res) {
-  last_data = [];
-})
-app.get('/pause',function(req, res) {
-  PAUSE = !PAUSE;
-})
+var ip_add = "http://192.168.0.105/";
+var url;
+var color = "black";
+var PORT = 9000;
 
-app.get('/data/:id',function(req, res) {
-  if(!PAUSE) {
-    const id = req.params.id;
-    var id_noise = Math.floor(id) - Math.floor(id) % 2;
-    last_data.push({temp: id_noise, time: get_time()});
-    res.send(last_data)
-  }else {
-    res.send("Monitoring paused!")
-  }
+
+
+expressApp.use(bodyParser());
+expressApp.use(bodyParser.json()); 
+expressApp.use(cors());
+
+expressApp.get('/fetch',function(req, res) {
+    res.send({color: color})
 })
 
-app.listen(PORT, function(req, res) {
-  console.log("Running server on port " +  PORT);
+expressApp.post('/fulfillment', app);
+
+app.intent('devicecontrol', (conv,{devicename,devicestatus}) => {
+    // speech = "I got "+lednumber+" and "+status;
+    var device_name = devicename;
+    var device_status = devicestatus;
+    color = device_status;
+     if(device_name == "fan" && device_status == "on"){
+         url = "D0/1";
+     }
+     else if(device_name == "fan" && device_status == "off"){
+         url = "D0/0";
+     }
+     else if(device_name == "light" && device_status == "on"){
+         url = "D1/1";
+     }
+     else if(device_name == "light" && device_status == "off"){
+         url = "D1/0";
+     }
+     else{
+        // conv.ask("Inputs not clear. Please renter your input.");
+     }
+    return new Promise((resolve, reject) => {
+        var options = { 
+            method: 'GET',
+            url: ip_add+url,
+            headers: 
+            { 
+                'cache-control': 'no-cache'
+            } 
+        };
+    
+            if (0) {
+                reject(conv.ask("I am not able to process it."));
+            }
+            else{
+                resolve(conv.ask("Turned "+device_status+ " "+device_name));
+            }
+           
+  });
 });
 
 
-function get_time() {
-  var dt = new Date();
-  var h = dt.getHours();
-  var m = dt.getMinutes();
-  var s = dt.getSeconds();
-  var time_decimal = (h+5 + (m+30)/60 + s/3600) % 24;
-  return time_decimal.toFixed(4);
-}
+expressApp.listen(PORT);
+console.log('Listening on port ' + PORT); 
+
